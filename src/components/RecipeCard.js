163a99f65b4247fbe8fc05473ce6f122c89ab6a1
Tyler Maschino <Tylermaschino@gmail.com>
2018-09-25 21:06:09 -0400

    Added recipe control and basic styling

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,55 +1,99 @@
-import React from "react";
+import React, { Component } from "react";
 import "../styles/RecipeCard.css";
 
-function RecipeCard(props) {
-  // If there is no data, don't worry be happy
-  const data = props.searchData ? props.searchData.data : "";
+class RecipeCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipeNumber: 0
+    };
+  }
 
-  // If there is data, let's get started
-  let recipeNumber = 0;
-  const recipeCount = data ? data.count : "";
-  const recipeData = data ? data.hits[recipeNumber].recipe : "";
-  const recipeTitle = data ? data.hits[recipeNumber].recipe.label : "";
-  const imgURL = data ? data.hits[recipeNumber].recipe.image : "";
-  const serving = data ? data.hits[recipeNumber].recipe.yield : "";
-  const recipeSourceName = data ? data.hits[recipeNumber].recipe.source : "";
-  const recipeSourceURL = data ? data.hits[recipeNumber].recipe.url : "";
-  const ingredients = data
-    ? data.hits[recipeNumber].recipe.ingredientLines
-    : "";
-  const calories = data ? data.hits[recipeNumber].recipe.calories : "";
+  // Shift to next recipe number
+  incrementRecipeNumber() {
+    let recipeNumber = this.state.recipeNumber;
+    if (recipeNumber < this.props.searchData.data.count) {
+      recipeNumber += 1;
+      this.setState({
+        recipeNumber: recipeNumber
+      });
+    }
+  }
 
-  console.log("Got the prop!");
-  console.log(data);
-  return (
-    <div className="recipe-card">
-      {data ? (
-        <div>
-          <h2 className="recipe-title">{recipeTitle}</h2>
-          <img src={imgURL} />
-          <h3>Nutrition Facts</h3>
-          <div className="nutrition-facts">
-            <p className="serving">Serving size: {serving}</p>
-            <p>Calories per serving: {Math.round(calories)}</p>
+  // Shift to prior recipe number
+  decrementRecipeNumber() {
+    let recipeNumber = this.state.recipeNumber;
+    if (recipeNumber > 0) {
+      recipeNumber -= 1;
+      this.setState({
+        recipeNumber: recipeNumber
+      });
+    }
+  }
+
+  render() {
+    // If there is no data, don't worry be happy
+    const data = this.props.searchData ? this.props.searchData.data : "";
+
+    // If there is data, let's get started
+    const recipeNumber = this.state.recipeNumber;
+    const recipeCount = data ? data.count : "";
+    const recipeData = data ? data.hits[recipeNumber].recipe : "";
+    const recipeTitle = data ? data.hits[recipeNumber].recipe.label : "";
+    const imgURL = data ? data.hits[recipeNumber].recipe.image : "";
+    const serving = data ? data.hits[recipeNumber].recipe.yield : "";
+    const recipeSourceName = data ? data.hits[recipeNumber].recipe.source : "";
+    const recipeSourceURL = data ? data.hits[recipeNumber].recipe.url : "";
+    const ingredients = data
+      ? data.hits[recipeNumber].recipe.ingredientLines
+      : "";
+    const calories = data ? data.hits[recipeNumber].recipe.calories : "";
+
+    return (
+      <div className="recipe-card">
+        {data ? (
+          <div>
+            <div className="recipe-control">
+              <button
+                onClick={() => this.decrementRecipeNumber()}
+              >{`<`}</button>
+              <span>
+                Recipe #{recipeNumber + 1}/{recipeCount}
+              </span>
+              <button
+                onClick={() => this.incrementRecipeNumber()}
+              >{`>`}</button>
+            </div>
+            <h2 className="recipe-title">{recipeTitle}</h2>
+            <img src={imgURL} className="recipe-image" />
+            <div className="nutrition-facts">
+              <h3>Nutrition Facts</h3>
+              <p className="serving">Serving size: {serving}</p>
+              <p>Calories per serving: {Math.round(calories)}</p>
+            </div>
+            <ul className="ingredients">
+              <h3>Ingredients</h3>
+              {ingredients.map(ingredient => (
+                <li key={ingredient.toString()}>{ingredient}</li>
+              ))}
+            </ul>
+            <div className="recipe-source">
+              <span>
+                <i>
+                  Source: <a href={recipeSourceURL}>{recipeSourceName}</a>
+                </i>
+              </span>
+            </div>
+            <div className="attribution">
+              <div id="edamam-badge" data-color="white" />
+            </div>
           </div>
-          <h3>Ingredients</h3>
-          <ul className="ingredient-list">
-            {ingredients.map(ingredient => (
-              <li>{ingredient}</li>
-            ))}
-          </ul>
-          <span>
-            <i>
-              Source: <a href={recipeSourceURL}>{recipeSourceName}</a>
-            </i>
-          </span>
-          <div id="edamam-badge" data-color="white" />
-        </div>
-      ) : (
-        ""
-      )}
-    </div>
-  );
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
 }
 
 export default RecipeCard;
